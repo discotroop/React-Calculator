69c9a626f16758a57fb73e82780b2be399ef7c62
John Mark Goodson <john.mark.hat@gmail.com>
2020-05-06 12:31:31 -0400

    Updates

---
+++ precache-manifest.3527fcfd189bf754767d3a0b5f4eee5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb76a89c75323c0505252045279328ab",
+    "url": "/git@github.com:discotroop/react-calculator.git/index.html"
+  },
+  {
+    "revision": "a37056a9daafb2cf40f7",
+    "url": "/git@github.com:discotroop/react-calculator.git/static/css/main.debe0f46.chunk.css"
+  },
+  {
+    "revision": "909158e7a8f396cd6389",
+    "url": "/git@github.com:discotroop/react-calculator.git/static/js/2.8be96392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git@github.com:discotroop/react-calculator.git/static/js/2.8be96392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a37056a9daafb2cf40f7",
+    "url": "/git@github.com:discotroop/react-calculator.git/static/js/main.217e6efe.chunk.js"
+  },
+  {
+    "revision": "4b5af97812bbf9481ef8",
+    "url": "/git@github.com:discotroop/react-calculator.git/static/js/runtime-main.81505db4.js"
+  }
+]);
